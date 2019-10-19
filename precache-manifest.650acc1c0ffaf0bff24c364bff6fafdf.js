adbbf09990a2397f6e56cf30b6e2c3b87139f15c
dhruvik7 <dhruvik.parikh@hotmail.com>
2019-10-18 22:26:52 -0700

    Updates

---
+++ precache-manifest.650acc1c0ffaf0bff24c364bff6fafdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd07cf045b11b092261f5d887611a849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7dc0c1373df77344f68",
+    "url": "/static/css/main.023e00f9.chunk.css"
+  },
+  {
+    "revision": "ffb975b6341de2f59236",
+    "url": "/static/js/2.1c88f69f.chunk.js"
+  },
+  {
+    "revision": "c7dc0c1373df77344f68",
+    "url": "/static/js/main.02aaed1c.chunk.js"
+  },
+  {
+    "revision": "3ee2b3a280eeda6336aa",
+    "url": "/static/js/runtime-main.521e139a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
--- src/store/actions.js
+++
@@ -1,6 +0,0 @@
-import { action } from "satcheljs";
-
-export let pickBreed = action("pickBreed", breed => ({ breed }));
-export let updateList = action("updateList");
-export let getDogs = action("getDogs");
-export let addPic = action("addPic", url => ({ url }));
